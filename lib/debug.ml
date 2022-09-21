439d0c86a4e15d596374f3eb2bd80e49deb22579
Francisco Ferreira <fferreira@gmail.com>
2022-09-21 13:27:15 -0400

    debug print when things happen

--- lib/debug.ml
+++ lib/debug.ml
@@ -1,30 +1,34 @@
 
+type flag =
+  { description : string
+  ; mutable state : bool
+  }
 
-
-let simplify_machine_flag = ref false
-let keep_only_reacheable_flag = ref false
-let project_to_empty_flag = ref false
-let post_process_taus_off_flag = ref false
-let minimise_off_flag = ref false
+let simplify_machine_flag = { description = "simplify_machine" ; state = false }
+let keep_only_reacheable_flag = { description = "keep_only_reacheable" ; state = false }
+let project_to_empty_flag = { description = "project_to_empty" ; state = false }
+let post_process_taus_off_flag = { description = "post_process_taus_off" ; state = false }
+let minimise_off_flag = { description = "minimise_off_flag" ; state = false }
 
 let set_all_debug_flags () =
-  simplify_machine_flag := true ;
-  keep_only_reacheable_flag := true ;
-  project_to_empty_flag := true ;
-  post_process_taus_off_flag := true ;
-  minimise_off_flag := true
+  simplify_machine_flag.state <- true ;
+  keep_only_reacheable_flag.state <- true ;
+  project_to_empty_flag.state <- true ;
+  post_process_taus_off_flag.state <- true ;
+  minimise_off_flag.state <- true
 
 let unset_all_debug_flags () =
-  simplify_machine_flag := false ;
-  keep_only_reacheable_flag := false ;
-  project_to_empty_flag := false ;
-  post_process_taus_off_flag := false ;
-  minimise_off_flag := false
+  simplify_machine_flag.state <- false ;
+  keep_only_reacheable_flag.state <- false ;
+  project_to_empty_flag.state <- false ;
+  post_process_taus_off_flag.state <- false ;
+  minimise_off_flag.state <- false
 
 
 let get_set_flag flag = function
-  | None -> !flag
-  | Some v -> let old = !flag in flag := v ; old
+  | None when flag.state = true -> "Skipping: " ^ flag.description |> Utils.log ; flag.state
+  | None -> "Doing: " ^ flag.description |> Utils.log ; flag.state
+  | Some v -> let old = flag.state in flag.state <- v ; old
 
 let simplify_machine_off  = get_set_flag simplify_machine_flag
 
